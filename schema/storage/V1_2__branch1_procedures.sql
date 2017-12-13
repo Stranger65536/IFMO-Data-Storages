c93275177e22f280075abe50e5f1af1a1cedaba8
Vladislav Trofimov <stranger.65536@gmail.com>
2017-12-14 01:04:46 +0300

    Branch 1 items backup & restore

--- schema/storage/V1_2__branch1_procedures.sql
+++ schema/storage/V1_2__branch1_procedures.sql
@@ -254,3 +254,256 @@ AS
       ');
   END;
 /
+CREATE OR REPLACE PROCEDURE BACKUP_BRANCH1_ITEMS
+AS
+  F_CHANGED_AT    TIMESTAMP := SYSDATE;
+  CURSOR ITEMS_CUR
+  IS
+    SELECT
+      ITEM_ID,
+      VENDOR_CODE,
+      NAME,
+      DESCRIPTION,
+      PRICE
+    FROM ITEMS@BRANCH_1
+    ORDER BY ITEM_ID ASC;
+
+  CURSOR ITEMS_STORE_CUR
+  IS
+    SELECT
+      ITEM_ID,
+      VENDOR_CODE,
+      NAME,
+      DESCRIPTION,
+      PRICE,
+      STATUS
+    FROM ITEMS_STORE CS1
+      INNER JOIN (
+                   SELECT
+                     ITEM_ID         C_ID,
+                     MAX(CHANGED_AT) CHANGED_AT
+                   FROM ITEMS_STORE
+                   GROUP BY ITEM_ID
+                 ) CS2
+        ON CS1.ITEM_ID = CS2.C_ID
+           AND CS1.CHANGED_AT = CS2.CHANGED_AT
+    ORDER BY ITEM_ID ASC;
+
+  ITEM_REC        ITEMS_CUR%ROWTYPE;
+  ITEMS_STORE_REC ITEMS_STORE_CUR%ROWTYPE;
+
+  FUNCTION IS_DELETED_ITEM(
+    RECORD_C ITEMS_STORE_CUR%ROWTYPE)
+    RETURN NUMBER
+  AS
+    REC_EXISTS NUMBER;
+    BEGIN
+      SELECT COUNT(*)
+      INTO REC_EXISTS
+      FROM ITEMS_STORE CS1
+        INNER JOIN (
+                     SELECT
+                       ITEM_ID         C_ID,
+                       MAX(CHANGED_AT) CHANGED_AT
+                     FROM ITEMS_STORE
+                     WHERE ITEM_ID = RECORD_C.ITEM_ID
+                     GROUP BY ITEM_ID
+                   ) CS2
+          ON CS1.ITEM_ID = CS2.C_ID
+             AND CS1.CHANGED_AT = CS2.CHANGED_AT
+      WHERE CS1.STATUS = 'D'
+            AND ROWNUM = 1;
+      IF (REC_EXISTS = 1)
+      THEN
+        BEGIN
+          DBMS_OUTPUT.PUT_LINE('ALREADY DELETED');
+          RETURN 1;
+        END;
+      ELSE
+        BEGIN
+          DBMS_OUTPUT.PUT_LINE('NOT DELETED');
+          RETURN 0;
+        END;
+      END IF;
+    END;
+
+  FUNCTION IS_EQUAL_ITEMS(
+    RECORD_C ITEMS_CUR%ROWTYPE,
+    RECORD_S ITEMS_STORE_CUR%ROWTYPE
+  )
+    RETURN NUMBER
+  AS
+    BEGIN
+      IF RECORD_C.ITEM_ID = RECORD_S.ITEM_ID
+         AND RECORD_C.VENDOR_CODE = RECORD_S.VENDOR_CODE
+         AND RECORD_C.NAME = RECORD_S.NAME
+         AND RECORD_C.DESCRIPTION = RECORD_S.DESCRIPTION
+         AND RECORD_C.PRICE = RECORD_S.PRICE
+      THEN
+        BEGIN
+          DBMS_OUTPUT.PUT_LINE('EQUAL');
+          RETURN 1;
+        END;
+      ELSE
+        BEGIN
+          DBMS_OUTPUT.PUT_LINE('NOT EQUAL');
+          RETURN 0;
+        END;
+      END IF;
+    END;
+
+  PROCEDURE INSERT_ITEM(
+    RECORD       ITEMS_CUR%ROWTYPE,
+    F_CHANGED_AT TIMESTAMP
+  ) AS
+    BEGIN
+      INSERT INTO ITEMS_STORE_TEMP
+      VALUES
+        (
+          F_CHANGED_AT,
+          RECORD.ITEM_ID,
+          1,
+          'I',
+          RECORD.VENDOR_CODE,
+          RECORD.NAME,
+          RECORD.DESCRIPTION,
+          RECORD.PRICE
+        );
+    END;
+
+  PROCEDURE UPDATE_ITEM(
+    RECORD       ITEMS_CUR%ROWTYPE,
+    F_CHANGED_AT TIMESTAMP
+  ) AS
+    BEGIN
+      INSERT INTO ITEMS_STORE_TEMP
+      VALUES
+        (
+          F_CHANGED_AT,
+          RECORD.ITEM_ID,
+          1,
+          'U',
+          RECORD.VENDOR_CODE,
+          RECORD.NAME,
+          RECORD.DESCRIPTION,
+          RECORD.PRICE
+        );
+    END;
+
+  PROCEDURE DELETE_ITEM(
+    RECORD       ITEMS_STORE_CUR%ROWTYPE,
+    F_CHANGED_AT TIMESTAMP
+  ) AS
+    BEGIN
+      INSERT INTO ITEMS_STORE_TEMP
+      VALUES
+        (
+          F_CHANGED_AT,
+          RECORD.ITEM_ID,
+          1,
+          'D',
+          NULL,
+          NULL,
+          NULL,
+          NULL
+        );
+    END;
+  BEGIN
+    OPEN ITEMS_CUR;
+    OPEN ITEMS_STORE_CUR;
+
+    FETCH ITEMS_CUR INTO ITEM_REC;
+    FETCH ITEMS_STORE_CUR INTO ITEMS_STORE_REC;
+
+    LOOP
+      EXIT WHEN ITEMS_CUR%NOTFOUND AND ITEMS_STORE_CUR%NOTFOUND;
+
+      IF (ITEMS_CUR%NOTFOUND)
+      THEN
+        ITEM_REC.ITEM_ID := NULL;
+      END IF;
+
+      IF (ITEMS_STORE_CUR%NOTFOUND)
+      THEN
+        ITEMS_STORE_REC.ITEM_ID := NULL;
+      END IF;
+
+      DBMS_OUTPUT.PUT_LINE('BRANCH:  ' || RAWTOHEX(ITEM_REC.ITEM_ID));
+      DBMS_OUTPUT.PUT_LINE('STORAGE: ' || RAWTOHEX(ITEMS_STORE_REC.ITEM_ID));
+
+      IF (ITEMS_STORE_REC.ITEM_ID IS NULL
+          OR ITEM_REC.ITEM_ID IS NOT NULL
+             AND ITEMS_STORE_REC.ITEM_ID IS NOT NULL
+             AND RAWTOHEX(ITEM_REC.ITEM_ID) < RAWTOHEX(ITEMS_STORE_REC.ITEM_ID))
+      THEN
+        BEGIN
+          DBMS_OUTPUT.PUT_LINE('INSERT ' || ITEM_REC.ITEM_ID);
+          INSERT_ITEM(ITEM_REC, F_CHANGED_AT);
+          FETCH ITEMS_CUR INTO ITEM_REC;
+        END;
+      ELSIF (ITEM_REC.ITEM_ID IS NULL
+             OR ITEM_REC.ITEM_ID IS NOT NULL
+                AND ITEMS_STORE_REC.ITEM_ID IS NOT NULL
+                AND RAWTOHEX(ITEM_REC.ITEM_ID) > RAWTOHEX(ITEMS_STORE_REC.ITEM_ID))
+        THEN
+          BEGIN
+            IF (IS_DELETED_ITEM(ITEMS_STORE_REC) = 0)
+            THEN
+              BEGIN
+                DBMS_OUTPUT.PUT_LINE('DELETE ' || ITEMS_STORE_REC.ITEM_ID);
+                DELETE_ITEM(ITEMS_STORE_REC, F_CHANGED_AT);
+              END;
+            END IF;
+            FETCH ITEMS_STORE_CUR INTO ITEMS_STORE_REC;
+          END;
+      ELSIF (RAWTOHEX(ITEM_REC.ITEM_ID) = RAWTOHEX(ITEMS_STORE_REC.ITEM_ID))
+        THEN
+          BEGIN
+            IF IS_EQUAL_ITEMS(ITEM_REC, ITEMS_STORE_REC) = 0
+            THEN
+              DBMS_OUTPUT.PUT_LINE('UPDATE ' || ITEM_REC.ITEM_ID);
+              UPDATE_ITEM(ITEM_REC, F_CHANGED_AT);
+            END IF;
+            FETCH ITEMS_CUR INTO ITEM_REC;
+            FETCH ITEMS_STORE_CUR INTO ITEMS_STORE_REC;
+          END;
+      END IF;
+    END LOOP;
+
+    CLOSE ITEMS_CUR;
+    CLOSE ITEMS_STORE_CUR;
+
+    INSERT INTO ITEMS_STORE SELECT *
+                            FROM ITEMS_STORE_TEMP;
+    DELETE FROM ITEMS_STORE_TEMP;
+  END;
+/
+CREATE OR REPLACE PROCEDURE RESTORE_BRANCH1_ITEMS(
+  F_CHANGED_AT TIMESTAMP)
+AS
+  BEGIN
+    EXECUTE IMMEDIATE ('DELETE FROM ITEMS@BRANCH_1');
+    EXECUTE IMMEDIATE ('
+        INSERT INTO ITEMS@BRANCH_1
+        SELECT
+          ITEM_ID,
+          VENDOR_CODE,
+          NAME,
+          DESCRIPTION,
+          PRICE
+        FROM ITEMS_STORE CS1
+          INNER JOIN (
+                       SELECT
+                         ITEM_ID     C_ID,
+                         MAX(CHANGED_AT) CHANGED_AT
+                       FROM ITEMS_STORE
+                       WHERE CHANGED_AT <= TO_TIMESTAMP(''' || TO_CHAR(F_CHANGED_AT, 'YYYY-MM-DD HH24:MI:SS') || ''', ''YYYY-MM-DD HH24:MI:SS'')
+                         AND STORE_NUMBER = 1
+                       GROUP BY ITEM_ID
+                     ) CS2
+            ON CS1.ITEM_ID = CS2.C_ID
+               AND CS1.CHANGED_AT = CS2.CHANGED_AT
+        WHERE STATUS <> ''D''
+      ');
+  END;
+/
